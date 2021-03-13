ba6c2113a2fa3f7e9e09fc5f569a7085d82268a8
Limenm <limenm2000@gmail.com>
2021-03-13 12:05:46 +0500

    Сделано отображение таблицы, добавление и удаление клиентов

---
+++ LANguage/Forms/UserForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace LANguage.Forms
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.patronymicBox = new System.Windows.Forms.TextBox();
+            this.birthdayBox = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.phoneBox = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.genderComboBox = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.createButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчество";
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Location = new System.Drawing.Point(15, 26);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(100, 20);
+            this.LastNameBox.TabIndex = 1;
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(121, 26);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameBox.TabIndex = 2;
+            // 
+            // patronymicBox
+            // 
+            this.patronymicBox.Location = new System.Drawing.Point(227, 26);
+            this.patronymicBox.Name = "patronymicBox";
+            this.patronymicBox.Size = new System.Drawing.Size(100, 20);
+            this.patronymicBox.TabIndex = 3;
+            // 
+            // birthdayBox
+            // 
+            this.birthdayBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.birthdayBox.Location = new System.Drawing.Point(334, 25);
+            this.birthdayBox.Mask = "00/00/0000";
+            this.birthdayBox.Name = "birthdayBox";
+            this.birthdayBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.birthdayBox.Size = new System.Drawing.Size(100, 20);
+            this.birthdayBox.TabIndex = 4;
+            this.birthdayBox.ValidatingType = typeof(System.DateTime);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(331, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Дата рождения";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Электронная почта";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(15, 85);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(100, 20);
+            this.emailBox.TabIndex = 11;
+            // 
+            // phoneBox
+            // 
+            this.phoneBox.Location = new System.Drawing.Point(122, 84);
+            this.phoneBox.Mask = "+7(999) 000-0000";
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneBox.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(118, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Телефон";
+            // 
+            // genderComboBox
+            // 
+            this.genderComboBox.FormattingEnabled = true;
+            this.genderComboBox.Items.AddRange(new object[] {
+            "Мужской",
+            "Женский"});
+            this.genderComboBox.Location = new System.Drawing.Point(227, 83);
+            this.genderComboBox.Name = "genderComboBox";
+            this.genderComboBox.Size = new System.Drawing.Size(121, 21);
+            this.genderComboBox.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(224, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Пол";
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(319, 131);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 23);
+            this.createButton.TabIndex = 16;
+            this.createButton.Text = "Создать";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(402, 131);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 17;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 166);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.genderComboBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.phoneBox);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.birthdayBox);
+            this.Controls.Add(this.patronymicBox);
+            this.Controls.Add(this.firstNameBox);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "UserForm";
+            this.Text = "User";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox LastNameBox;
+        public System.Windows.Forms.TextBox firstNameBox;
+        public System.Windows.Forms.TextBox patronymicBox;
+        public System.Windows.Forms.MaskedTextBox birthdayBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.TextBox emailBox;
+        public System.Windows.Forms.MaskedTextBox phoneBox;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.ComboBox genderComboBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ LANguage/Tables/UserTable.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using System.Windows.Forms;
+using LANguage.Forms;
+
+namespace LANguage.Tables
+{
+    public partial class UserTable : Page
+    {
+        LangaugeDB db = new LangaugeDB();
+        Client clientForDelite;
+        public UserTable()
+        {
+            InitializeComponent();
+            db.Client.Load();
+            listBoxTableClient.ItemsSource = db.Client.Local.ToBindingList();
+            
+        }
+
+        private void createTupleButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserForm userForm = new UserForm();
+            if (userForm.ShowDialog() == DialogResult.OK)
+            {
+                Client client= new Client();
+                Client client1 = new Client();
+                client1 = db.Client.OrderByDescending(o => o.ID).FirstOrDefault();
+                client.ID = client1.ID + 1;
+                client.FirstName = userForm.firstNameBox.Text;
+                client.LastName = userForm.LastNameBox.Text;
+                client.Patronymic = userForm.patronymicBox.Text;
+                client.Birthday = Convert.ToDateTime(userForm.birthdayBox.Text);
+                client.RegistrationDate = DateTime.Today;
+                client.Email = userForm.emailBox.Text;
+                client.Phone = userForm.phoneBox.Text;
+                if (userForm.genderComboBox.Text == "Мужской")
+                {
+                    client.GenderCode = "1";
+                }
+                else if (userForm.genderComboBox.Text == "Женский")
+                {
+                    client.GenderCode = "0";
+                }
+                db.Client.Add(client);
+                db.SaveChanges();
+                userForm.Dispose();
+            }
+            
+        }
+
+        private void deleteTupleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Client client = db.Client.Where(o => o.ID == clientForDelite.ID).FirstOrDefault();
+            db.Client.Remove(client);
+            db.SaveChanges();
+
+        }
+
+        public void listBoxTableClient_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            clientForDelite = listBoxTableClient.SelectedItem as Client;
+        }
+    }
+}
